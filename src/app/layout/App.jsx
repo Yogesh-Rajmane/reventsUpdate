f30f3469467f6e7030e9986efeba7e70ecc0a2e1
Yogesh Rajmane <yogesh.rajmane91@gmail.com>
2020-06-13 12:31:28 +0530

    End section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,12 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component,Fragment } from 'react'
+import NavBar from '../../features/Nav/NavBar/NavBar';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import { Container } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Revents Apps</h1>
-        <h3>Git Testing</h3>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          
+          <EventDashboard />
+       </Container>
+    </Fragment>
+      
     )
   }
 }
