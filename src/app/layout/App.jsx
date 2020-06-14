910cdf24ac3bc3f10d7887b64d8a774eead1b1d8
Yogesh Rajmane <yogesh.rajmane91@gmail.com>
2020-06-14 14:12:42 +0530

    End Of Section 6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -2,18 +2,37 @@ import React, { Component,Fragment } from 'react'
 import NavBar from '../../features/Nav/NavBar/NavBar';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDahsboard';
+import UserDeatailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-        <Container className="main">
-          
-          <EventDashboard />
-       </Container>
-    </Fragment>
-      
+            <Route exact path='/' component={HomePage} />
+            <Route 
+                path='/(.+)' 
+                render={()=> (
+                <Fragment>
+                  <NavBar />
+                    <Container className="main">
+
+                      <Route path='/events' component={EventDashboard} />  
+                      <Route path='/events/:id' component={EventDetailedPage} />  
+                      <Route path='/people' component={PeopleDashboard} />  
+                      <Route path='/profile/:id' component={UserDeatailedPage} />
+                      <Route path='/settings' component={SettingsDashboard} />
+                      <Route path='/createEvent' component={EventForm} />  
+                  </Container>
+                </Fragment>)} />  
+      </Fragment>
+    
+    
     )
   }
 }
---
+++ src/features/user/PeopleDashboard/PeopleDahsboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PeopleDahsboard = () => {
+    return (
+        <div>
+            <h1>PeopleDahsboard</h1>
+        </div>
+    )
+}
+
+export default PeopleDahsboard;
